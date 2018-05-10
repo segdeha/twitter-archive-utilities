3adec95b8ae32879c9e60b0015fb9d22a896b05b
Andrew Hedges <andrew@hedges.name>
2018-05-10 15:09:31 -0700

    Save deleted tweet IDs to a file to avoid making requests for the same tweets multiple times

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const Twitter = require('./lib/twitter')
 const Moment = require('moment')
 const dir = require('node-dir')
+const fs = require('fs')
 const Promise = require('bluebird')
 
 let tweets = []
@@ -28,19 +29,35 @@ function handleEndOfFiles(err, files) {
     filterRetweets()
 }
 
+function getPreviouslyDeletedRetweets() {
+    let contents = fs.readFileSync('./deleted-retweet-ids.txt', 'utf8')
+    let deletedRetweets = contents.split('\n')
+    return deletedRetweets
+}
+
 function filterRetweets() {
-    console.log('Total tweets:', tweets.length)
+    let deletedRetweets = getPreviouslyDeletedRetweets()
+    console.log(`Total tweets for user ${process.env.TWITTER_USERNAME}:`, tweets.length)
+
     let retweets = tweets.filter(tweet => {
         return typeof tweet.retweeted_status !== 'undefined'
     })
     console.log(`Total retweets:`, retweets.length)
+
     let oldRetweets = retweets.filter(tweet => {
         return new Moment().diff(new Moment(new Date(tweet.created_at)), 'days') >= process.env.RETWEETS_MAX_DAYS
     })
     console.log(`Retweets older than ${process.env.RETWEETS_MAX_DAYS} days:`, oldRetweets.length)
+
+    let undeletedRetweets = oldRetweets.filter(tweet => {
+        // filter out IDs that are in the list of previously deleted tweets
+        return deletedRetweets.indexOf(tweet.id_str) < 0
+    })
+    console.log(`Undeleted old retweets:`, undeletedRetweets.length)
+
     if ('true' === process.env.RETWEETS_DELETE) {
-        if (oldRetweets.length > 0) {
-            deleteRetweets(oldRetweets)
+        if (undeletedRetweets.length > 0) {
+            deleteRetweets(undeletedRetweets)
         }
     }
     else {
@@ -56,7 +73,15 @@ function deleteRetweets(retweets) {
 function deleteRetweet(retweet) {
     let id = retweet.id_str
     return Twitter.request(`statuses/destroy/${id}`, 'POST', { id })
-        .then(deletedRetweet => { console.log(`Deleted retweet with ID: ${id}`) })
+        .then(deletedRetweet => {
+            console.log(`Deleted retweet with ID: ${id}`)
+            // write to file so we can avoid making requests for already deleted retweets
+            try {
+                fs.appendFileSync('./deleted-retweet-ids.txt', `${id}\n`, 'utf8')
+            } catch (err) {
+                console.log('Problem writing to file:', err)
+            }
+        })
         .catch(error => { console.error(`Could not delete retweet with ID ${id} because ${error}`) })
 }
 
